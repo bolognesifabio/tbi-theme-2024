9f5ee3bbdfd2bac55defe07030f9b01bef1332c3
The Webolution <val.senneca@gmail.com>
2019-01-08 00:18:09 +0100

    Fixed capabilities for clubs

--- wp-content/plugins/tbi-plugin/taxonomies/_clubs.php
+++ wp-content/plugins/tbi-plugin/taxonomies/_clubs.php
@@ -3,14 +3,19 @@ register_taxonomy(
     'clubs',
     ['teams'],
     [
-        'hierarchical' => true,
         'labels' => [
             'name' => _x('Società', 'taxonomy general name'),
             'singular_name' => _x('Società', 'taxonomy singular name'),
         ],
+        'hierarchical' => true,
         'show_ui' => true,
         'show_admin_column' => true,
         'query_var' => true,
         'rewrite' => true,
+        'capabilities' => [
+            'manage_terms' => 'manage_clubs_terms',
+            'edit_terms' => 'edit_clubs_terms',
+            'delete_terms' => 'delete_clubs_terms'
+        ]
     ]
 );
