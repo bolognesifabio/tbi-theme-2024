e81b94c0b366d52277697e9734816b2a052268cf
The Webolution <val.senneca@gmail.com>
2019-01-27 19:55:19 +0100

    Fixed css classes

--- wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_filters.php
+++ wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_filters.php
@@ -1,14 +1,14 @@
 <h3>Selezione squadre</h3>
 
 <tbi-competitions-filters inline-template>
-    <ul class="competitions-metaboxes__teams__filters">
-        <tbi-field label="Mostra solo squadre attive" class="competitions-metaboxes__teams__filters__field">
+    <ul :class="base_class">
+        <tbi-field label="Mostra solo squadre attive">
             <tbi-switch-checkbox>
                 <input slot-scope="slot_props" :class="slot_props.bem_class" :type="slot_props.type" v-model="$root.state.are_inactive_hidden" />
             </tbi-switch-checkbox>
         </tbi-field>
 
-        <tbi-field label="Mostra solo squadre selezionate" class="competitions-metaboxes__teams__filters__field">
+        <tbi-field label="Mostra solo squadre selezionate">
             <tbi-switch-checkbox>
                 <input slot-scope="slot_props" :class="slot_props.bem_class" :type="slot_props.type" v-model="$root.state.are_unselected_hidden" />
             </tbi-switch-checkbox>
--- wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_teams-list.php
+++ wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_teams-list.php
@@ -1,18 +1,18 @@
 <tbi-competitions-teams-list inline-template :teams_input='<?= htmlspecialchars(json_encode($all_teams), ENT_QUOTES) ?>'>
-    <ul class="competitions-metaboxes__teams__participants">
-        <li v-for="team in $root.state.teams" v-if="is_team_visible(team)" class="competitions-metaboxes__teams__participants__item">
+    <ul :class="base_class">
+        <li v-for="team in $root.state.teams" v-if="is_team_visible(team)" :class="get_bem('item')">
             <input
                 v-model="team.is_selected"
-                class="competitions-metaboxes__teams__participants__item__checkbox"
                 type="checkbox"
+                :class="get_bem('item__checkbox')"
                 :name="'tbi-metaboxes-competitions-teams-participants[' + team.id + ']'"
                 :value="team.id"
             />
-            <div class="competitions-metaboxes__teams__participants__item__checkbox-interface dashicons dashicons-yes"></div>
-            <div class="competitions-metaboxes__teams__participants__item__emblem">
-                <img class="competitions-metaboxes__teams__participants__item__emblem__image" :src="team.emblem" />
+            <div :class="[get_bem('item__checkbox-interface dashicons'), 'dashicons-yes']"></div>
+            <div :class="get_bem('item__emblem')">
+                <img :class="get_bem('item__emblem__image')" :src="team.emblem" />
             </div>
-            <label class="competitions-metaboxes__teams__participants__item__name">{{ team.title }}</label>
+            <label :class="get_bem('item__name')">{{ team.title }}</label>
         </li>
         <li v-if="!is_at_least_one_team_visible">Nessun team visibile. Prova a disattivare i filtri.</li>
     </ul>
