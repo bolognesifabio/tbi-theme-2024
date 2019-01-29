3b382a3670365cc005ace7e17b904c2533314bb1
The Webolution <val.senneca@gmail.com>
2019-01-29 22:20:28 +0100

    Team info scrollable table

--- wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_teams-info.php
+++ wp-content/plugins/tbi-plugin/metaboxes/competitions/teams/_teams-info.php
@@ -4,34 +4,44 @@ const INPUTS_NAME_PREFIX = 'tbi-metaboxes-competitions-teams-info-'; ?>
 <tbi-competitions-teams-info inline-template>
     <div v-if="is_at_least_one_team_selected" :class="base_class">
         <h3 :class="get_bem('title')">Informazioni per questa competizione</h3>
-        <table :class="get_bem('table')">
-            <thead :class="get_bem('info__headings')">
-                <tr>
-                    <th :class="get_bem('info__headings__text')" colspan="2">Squadra</th>
-                    <th :class="get_bem('info__headings__text')">Nome</th>
-                    <th :class="get_bem('info__headings__text')">Nome breve</th>
-                    <th :class="get_bem('info__headings__text')">Sigla</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="team in $root.state.teams" v-if="team.is_selected" :class="get_bem('info__team')">
-                    <td :class="get_bem('info__team__emblem')">
-                        <img :src="team.emblem" :class="get_bem('info__team__emblem__image')" />
-                    </td>
-                    <td :class="get_bem('info__team__title')">
-                        <label>{{ team.title }}</label>
-                    </td>
-                    <td :class="get_bem('info__team__name')">
-                        <input :name="'<?= INPUTS_NAME_PREFIX ?>name[' + team.id + ']'" type="text" v-model="team.competition_info.name" />
-                    </td>
-                    <td :class="get_bem('info__team__short-name')">
-                        <input :name="'<?= INPUTS_NAME_PREFIX ?>short-name[' + team.id + ']'" type="text" v-model="team.competition_info.short_name" />
-                    </td>
-                    <td :class="get_bem('info__team__team-code')">
-                        <input :name="'<?= INPUTS_NAME_PREFIX ?>team-code[' + team.id + ']'" type="text" v-model="team.competition_info.team_code" maxlength="3" />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div v-if="is_at_least_one_team_selected" :class="get_bem('scroll-container')">
+            <table :class="get_bem('scroll-container__table')">
+                <thead :class="get_bem('scroll-container__table__headings')">
+                    <tr>
+                        <th :class="get_bem('scroll-container__table__headings__text')" colspan="2">Squadra</th>
+                        <th :class="get_bem('scroll-container__table__headings__text')">Nome</th>
+                        <th :class="get_bem('scroll-container__table__headings__text')">Nome breve</th>
+                        <th :class="get_bem('scroll-container__table__headings__text') + ' narrow'">Sigla</th>
+                        <th :class="get_bem('scroll-container__table__headings__text') + ' narrow'">Penalit&agrave;</th>
+                        <th :class="get_bem('scroll-container__table__headings__text') + ' narrow'">Priorit&agrave;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="team in $root.state.teams" v-if="team.is_selected" :class="get_bem('scroll-container__table__team')">
+                        <th :class="get_bem('scroll-container__table__team__emblem')">
+                            <img :src="team.emblem" :class="get_bem('scroll-container__table__team__emblem__image')" />
+                        </th>
+                        <th :class="get_bem('scroll-container__table__team__title')">
+                            <label>{{ team.title }}</label>
+                        </th>
+                        <td :class="get_bem('scroll-container__table__team__input')">
+                            <input :name="'<?= INPUTS_NAME_PREFIX ?>name[' + team.id + ']'" type="text" v-model="team.competition_info.name" />
+                        </td>
+                        <td :class="get_bem('scroll-container__table__team__input')">
+                            <input :name="'<?= INPUTS_NAME_PREFIX ?>short-name[' + team.id + ']'" type="text" v-model="team.competition_info.short_name" />
+                        </td>
+                        <td :class="get_bem('scroll-container__table__team__input') + ' narrow'">
+                            <input :name="'<?= INPUTS_NAME_PREFIX ?>team-code[' + team.id + ']'" type="text" v-model="team.competition_info.team_code" maxlength="3" />
+                        </td>
+                        <td :class="get_bem('scroll-container__table__team__input') + ' narrow'">
+                            <input :name="'<?= INPUTS_NAME_PREFIX ?>team-code[' + team.id + ']'" type="text" v-model="team.competition_info.team_code" maxlength="3" />
+                        </td>
+                        <td :class="get_bem('scroll-container__table__team__input') + ' narrow'">
+                            <input :name="'<?= INPUTS_NAME_PREFIX ?>team-code[' + team.id + ']'" type="text" v-model="team.competition_info.team_code" maxlength="3" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </tbi-competitions-teams-info>
