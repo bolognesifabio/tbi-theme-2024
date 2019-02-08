94cd10378206b546cd1cd818614d5f07f2bb43e2
Valerio Senneca <val.senneca@gmail.com>
2019-02-08 17:25:05 +0100

    Removed useless template

--- src/plugin/components/metaboxes/competitions/turns-list/fixture/index.js
+++ src/plugin/components/metaboxes/competitions/turns-list/fixture/index.js
@@ -1,11 +1,9 @@
 import props from './_props'
 import methods from './_methods'
-import template from './_template'
 
 export default {
     props,
     methods,
-    template,
     components: {
         'tbi-fixture-team-selection': () => import('../team-selection')
     }
