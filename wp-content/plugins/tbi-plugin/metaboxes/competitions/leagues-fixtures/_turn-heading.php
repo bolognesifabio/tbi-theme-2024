3f8701ccbe46fcaf78fc286afc4d4fd34ade19f2
Valerio Senneca <val.senneca@gmail.com>
2019-02-11 17:05:40 +0100

    Begin styling turns

--- wp-content/plugins/tbi-plugin/metaboxes/competitions/leagues-fixtures/_turn-heading.php
+++ wp-content/plugins/tbi-plugin/metaboxes/competitions/leagues-fixtures/_turn-heading.php
@@ -1,16 +1,19 @@
-<div :class="get_bem('heading')">
-    <h4
+<div
+    :class="get_bem('heading')"
+    @dragover="event => prevent_if_droppable(event)"
+    @dragleave="remove_dragged_over_class"
+    @drop.prevent="drop_turn"
+>
+
+    <div
         :class="get_bem('heading__drag-area')"
-        @dragover="event => prevent_if_droppable(event)"
-        @dragleave="remove_dragged_over_class"
-        @drop.prevent="drop_turn"
         @dragend="() => is_currently_dragged = false"
         @dragstart="update_turns_dragged_data"
         draggable="true"
-    >{{ turn.name }}</h4>
+    ></div>
 
     <input :class="get_bem('heading__input')" v-model="turn.name" />
 
-    <button :class="get_bem('heading__button', 'delete')" @click.prevent="remove_turn()">X</button>
+    <button :class="get_bem('heading__button', 'delete')" @click.prevent="remove_turn()"></button>
     <button :class="get_bem('heading__button', 'add')" @click.prevent="add_fixture()">Aggiungi nuova partita</button>
 </div>
