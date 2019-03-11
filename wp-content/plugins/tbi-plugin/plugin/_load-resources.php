79c32c10958a9c3311682037a180c16c1dabb19b
The Webolution <val.senneca@gmail.com>
2019-03-12 00:45:40 +0100

    Added critical enqueue to plugin

--- wp-content/plugins/tbi-plugin/plugin/_load-resources.php
+++ wp-content/plugins/tbi-plugin/plugin/_load-resources.php
@@ -1,3 +1,4 @@
 <?php
 $dist_folder_hash = scandir('../wp-content/plugins/tbi-plugin/dist', 1)[0];
 wp_enqueue_script('tbi-admin-script', plugin_dir_url(__FILE__) . '../dist/' . $dist_folder_hash . '/main.js', null, null, true);
+wp_enqueue_style('tbi-admin-critical-style', plugin_dir_url(__FILE__) . '../dist/' . $dist_folder_hash . '/critical.css');
