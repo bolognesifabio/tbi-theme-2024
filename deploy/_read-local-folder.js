fec080493d26e8c48c8021c6249a032656e83f31
Valerio Senneca <val.senneca@gmail.com>
2019-03-22 13:44:12 +0100

    Fixing deploy script

--- deploy/_read-local-folder.js
+++ deploy/_read-local-folder.js
@@ -1,31 +1,20 @@
 const fs = require('fs-extra')
 
-const read_local_folder = async function(directory) {
+const read_local_folder = function(directory) {
+    const FOLDER_CONTENT = fs.readdirSync(directory)
     let all_files = []
+    
+    FOLDER_CONTENT.forEach(resource => {
+        const
+            RESOURCE_PATH = `${directory}/${resource}`,
+            RESOURCE_STAT = fs.statSync(RESOURCE_PATH),
+            IS_DIRECTORY = RESOURCE_STAT && RESOURCE_STAT.isDirectory()
 
-    await fs.readdir(directory, async (error, resources) => {
-        await resources.forEach(async resource => {
-            const RESOURCE_PATH = `${directory}/${resource}`
-
-            await fs.stat(RESOURCE_PATH, (stat_error, stat) => {
-                const IS_DIRECTORY = stat && stat.isDirectory()
-
-                if (IS_DIRECTORY && all_files.push) {
-                    all_files.push('/')
-
-                    console.log(all_files.length)
-                    // console.log("\n\n")
-                    // all_files.push(...read_local_folder(RESOURCE_PATH))
-                }
-                else all_files.push(RESOURCE_PATH)
-            })
-        })
+        if (IS_DIRECTORY) all_files.push(...read_local_folder(RESOURCE_PATH))
+        else all_files.push(RESOURCE_PATH)
     })
 
-    console.log('asd')
-
     return all_files
-
 }
 
 module.exports = read_local_folder
--- deploy/index.js
+++ deploy/index.js
@@ -1,10 +1,80 @@
-const read_local_folder = require('./_read-local-folder')
+const
+    read_local_folder = require('./_read-local-folder'),
+    LOCAL_FOLDER_FILES = read_local_folder('wp-content/plugins/tbi-plugin')
+
+let
+    Client = require('@icetee/ftp'),
+    fs = require('fs-extra'),
+    ftp = new Client()
+
+console.log(LOCAL_FOLDER_FILES.length)
 
 
-const test = async function() {
-    let all_files = await read_local_folder('wp-content/plugins/tbi-plugin')
-    console.log('--------------')
-    console.log(all_files)
+
+const CONNECTION_CONFIG = {
+    host: "127.0.0.1",
+    user: "admin",
+    password: "1",
+    port: 21
+}
+// let dir = "wp-content/plugins/tbi-plugin"
+
+ftp.connect(CONNECTION_CONFIG)
+        // ftp.list("test-ftp", (error, files) => {
+        //     files.forEach(file => {
+        //         console.log(file.name)
+        //     })
+        // })
+
+        // ftp.end()
+
+
+const read_remote_folder = (ftp, directory) => {
+    return new Promise(resolve => {
+        ftp.list(directory, (error, resources) => {
+            // resources.forEach(resource => {
+            //     const RESOURCE_PATH = `${directory}/${resource.name}`
+
+            //     if (resource.type === 'd') {
+            //         Promise.all([read_remote_folder(ftp, RESOURCE_PATH)])
+            //             .then(res => {
+            //                 all_files.push(res)
+            //             })
+            //     }
+            //     else all_files.push(RESOURCE_PATH) 
+            // })
+
+            console.log(resources.map(resource => {
+                const RESOURCE_PATH = `${directory}/${resource.name}`
+
+                if (resource.type === 'd') {
+                    return read_remote_folder(ftp, RESOURCE_PATH)
+                }
+                else return RESOURCE_PATH
+            }))
+            // resolve(Promise.all(resources.map(resource => {
+            //     const RESOURCE_PATH = `${directory}/${resource.name}`
+
+            //     if (resource.type === 'd') {
+            //         return read_remote_folder(ftp, RESOURCE_PATH)
+            //     }
+            //     else return RESOURCE_PATH
+            // })))
+        })
+    })
+
 }
 
-test()
+// console.log(read_remote_folder(ftp, "test-ftp"))
+
+// read_remote_folder(ftp, "test-ftp")
+//     .then(remote_files => {
+//         console.log(remote_files)
+//     })
+
+Promise.all([read_remote_folder(ftp, "test-ftp")])
+    .then(remote_files => {
+        console.log(remote_files)
+    })
+
+ftp.end()
