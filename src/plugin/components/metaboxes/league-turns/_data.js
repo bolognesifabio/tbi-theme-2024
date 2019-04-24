bfb35e4422ea602a9cd10a35b2827861a6736a08
Valerio Senneca <val.senneca@gmail.com>
2019-04-24 14:38:43 +0200

    Drag and drop for fixtures

--- src/plugin/components/metaboxes/league-turns/_data.js
+++ src/plugin/components/metaboxes/league-turns/_data.js
@@ -1,5 +1,6 @@
 export default function() {
     return {
-        current_dragged: null
+        current_dragged_turn: null,
+        current_dragged_fixture: null
     }
 }
--- src/plugin/components/metaboxes/league-turns/_methods.js
+++ src/plugin/components/metaboxes/league-turns/_methods.js
@@ -6,23 +6,51 @@ export default {
         })
     },
 
-    switch_turns(index) {
-        const IS_DRAGGABLE_MOVING_DOWN = index > this.current_dragged.index
-        this.$root.state.turns.splice(index + IS_DRAGGABLE_MOVING_DOWN, 0, this.current_dragged.turn)
-        this.$root.state.turns.splice(this.current_dragged.index + !IS_DRAGGABLE_MOVING_DOWN, 1)
-        this.current_dragged.index = index
-    },
-    
-    set_current_dragged(index, turn) {
-        this.current_dragged = { index, turn }
+    switch_turns(event, index) {
+        if (this.current_dragged_turn) {
+            const IS_DRAGGABLE_MOVING_DOWN = index > this.current_dragged_turn.index
+            
+            event.preventDefault()
+
+            this.$root.state.turns.splice(index + IS_DRAGGABLE_MOVING_DOWN, 0, this.current_dragged_turn.turn)
+            this.$root.state.turns.splice(this.current_dragged_turn.index + !IS_DRAGGABLE_MOVING_DOWN, 1)
+            this.current_dragged_turn.index = index
+        }
     },
 
-    reset_current_dragged() {
-        this.current_dragged = null
+    switch_fixtures(event, turn_index, fixture_index) {
+        if (this.current_dragged_fixture) {
+            const
+                IS_MOVING_TO_ANOTHER_TURN = turn_index !== this.current_dragged_fixture.turn_index,
+                IS_DRAGGABLE_MOVING_DOWN = fixture_index > this.current_dragged_fixture.fixture_index
+
+            event.preventDefault()
+
+            this.$root.state.turns[turn_index].fixtures.splice(fixture_index + (!IS_MOVING_TO_ANOTHER_TURN && IS_DRAGGABLE_MOVING_DOWN), 0, this.current_dragged_fixture.fixture)
+            this.$root.state.turns[this.current_dragged_fixture.turn_index].fixtures.splice(this.current_dragged_fixture.fixture_index + (IS_MOVING_TO_ANOTHER_TURN ? 0 : !IS_DRAGGABLE_MOVING_DOWN), 1)
+            this.current_dragged_fixture.fixture_index = fixture_index
+            this.current_dragged_fixture.turn_index = turn_index
+        }
+    },
+    
+    set_current_dragged_turn(index, turn) {
+        this.current_dragged_turn = { index, turn }
+    },
+
+    reset_current_dragged_turn() {
+        this.current_dragged_turn = null
+    },
+
+    set_current_dragged_fixture(turn_index, fixture_index, fixture) {
+        this.current_dragged_fixture = { turn_index, fixture_index, fixture }
+    },
+
+    reset_current_dragged_fixture() {
+        this.current_dragged_fixture = null
     },
 
     is_turn_dragged(index) {
-        return this.current_dragged && this.current_dragged.index === index
+        return this.current_dragged_turn && this.current_dragged_turn.index === index
     },
 
     toggle_open_status(index) {
