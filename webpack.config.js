d17047aa40d4e3d6c1386c5e3bd7b8c654df3ea1
Valerio Senneca <val.senneca@gmail.com>
2019-05-14 14:33:45 +0200

    Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,129 +1,86 @@
-// const
-//     PACKAGE_JSON = require('./package.json'),
-//     Clean_Webpack_Plugin = require('clean-webpack-plugin'),
-//     Mini_Css_Extract_Plugin = require('mini-css-extract-plugin'),
-//     path = require('path')
-
-// module.exports = {
-//     entry: ['./assets/src/public/theme-manifest.css'],
-//     output: {
-//         path: path.resolve(__dirname, 'assets/')
-//     },
-//     plugins: [
-//         new Clean_Webpack_Plugin('assets/css/style.css'),
-//         new Mini_Css_Extract_Plugin({
-//             filename: `style.css`
-//         })
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /theme-manifest\.css$/,
-//                 use: [Mini_Css_Extract_Plugin.loader, 'css-loader']
-//             },
-//             {
-//                 test: /theme-manifest\.css$/,
-//                 loader: 'string-replace-loader',
-//                 options: {
-//                     search: '[theme-version]',
-//                     replace: PACKAGE_JSON.version
-//                 }
-//             }
-//         ]
-//     }
-// }
-
 const
-    IS_MODE_PRODUCTION = process.argv[2] === '--mode=production',
-    PACKAGE_JSON = require('./package.json'),
     path = require('path'),
     Clean_Webpack_Plugin = require('clean-webpack-plugin'),
     Mini_Css_Extract_Plugin = require('mini-css-extract-plugin'),
+    Extract_Text_Plugin = require('extract-text-webpack-plugin'),
     Git_Revision_Plugin = require('git-revision-webpack-plugin'),
-    Write_Json_Plugin = require('write-json-webpack-plugin')
+    Write_Json_Plugin = require('write-json-webpack-plugin'),
+    PACKAGE_JSON = require('./package.json'),
+    IS_MODE_PRODUCTION = process.argv[2] === '--mode=production'
 
-let git_revision = new Git_Revision_Plugin({
-    lightweightTags: true
-})
+let git_revision = new Git_Revision_Plugin({ lightweightTags: true })
 
-const get_config = context => {
-    return {
-        mode: IS_MODE_PRODUCTION ? 'production' : 'development',
-        entry: [
-            `./assets/src/${context}/index.js`,
-            `./assets/src/${context}/style/critical/index.scss`
-        ],
-        output: {
-            publicPath: `/assets/${context}/`,
-            path: path.resolve(__dirname, `assets/${context}`),
-            filename: `index.js`,
-            chunkFilename: `chunks/[chunkhash].js`
-        },
-        plugins: [
-            new Clean_Webpack_Plugin(`assets/${context}`),
-            new Mini_Css_Extract_Plugin({
-                filename: `style.css`
-            }),
-            new Write_Json_Plugin({
-                object: {
-                    hash: git_revision.commithash()
-                },
-                filename: 'version.json'
-            })
-        ],
-        module: {
-            rules: [
-                {
-                    test: /.*.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015']
-                        }
-                    }
-                },
-                {
-                    test: /\.scss$/,
-                    exclude: /critical/,
-                    use: ['style-loader', 'css-loader', 'sass-loader']
-                },
-                {
-                    test: /\.scss$/,
-                    include: /critical/,
-                    use: [Mini_Css_Extract_Plugin.loader, 'css-loader', 'sass-loader']
-                },
-                {
-                    test: /\.(png|jpg|gif|svg)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: `[name].[ext]?ver=`,
-                                outputPath: 'img/'
-                            }
-                        },
-                    ],
-                },
-                {
-                    test: /theme-manifest\.css$/,
-                    loader: 'string-replace-loader',
+module.exports = {
+    mode: IS_MODE_PRODUCTION ? 'production' : 'development',
+    entry: {
+        'admin': ['./assets/src/admin/index.js', './assets/src/admin/style/critical/index.scss'],
+        'public': ['./assets/src/public/index.js', './assets/src/public/style/critical/index.scss'],
+        'style': './assets/src/theme-manifest.css',
+    },
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'assets/dist/[name].js',
+        chunkFilename: `assets/dist/chunks/[chunkhash].js`
+    },
+    plugins: [
+        new Extract_Text_Plugin('style.css'),
+        new Mini_Css_Extract_Plugin({ filename: 'assets/dist/[name].css' }),
+        new Clean_Webpack_Plugin({
+            cleanOnceBeforeBuildPatterns: ['assets/dist', 'style.css'],
+            cleanAfterEveryBuildPatterns: ['assets/dist/style.js'],
+            protectWebpackAssets: false
+        }),
+        new Write_Json_Plugin({
+            object: {
+                hash: git_revision.commithash(),
+                version: PACKAGE_JSON.version
+            },
+            filename: 'assets/dist/version.json'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /.*.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        search: '[theme-version]',
-                        replace: PACKAGE_JSON.version,
+                        presets: ['es2015']
                     }
                 }
-            ]
-        },
-        resolve: {
-            alias: {
-                vue: IS_MODE_PRODUCTION ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /critical/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.scss$/,
+                include: /critical/,
+                use: [Mini_Css_Extract_Plugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /theme-manifest\.css$/,
+                use: Extract_Text_Plugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'string-replace-loader',
+                            options: {
+                                search: '[theme-version]',
+                                replace: PACKAGE_JSON.version
+                            }
+                        }
+                    ]
+                })
             }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue: IS_MODE_PRODUCTION ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
         }
     }
 }
-
-module.exports = [
-    get_config('admin'),
-    get_config('public')
-]
