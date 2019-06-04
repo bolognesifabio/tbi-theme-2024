fd4ae94ce0f61c6b9054133f7031cd6348c07142
Valerio Senneca <val.senneca@gmail.com>
2019-06-04 14:23:29 +0200

    Fixed standings API

--- app/api/standings-by-terms.php
+++ app/api/standings-by-terms.php
@@ -3,7 +3,10 @@ namespace TBI\API;
 use TBI\Models\API_Route;
 use TBI\Controllers\Competition\League;
 
-$route = '/competitions/standings/(?P<competition>\d+)/(?P<season>\d+)';
-$callback = League::get_standings_by_terms;
+class League_API_Controller {
+    public function get_standings_by_terms($data) {
+        return League::get_standings_by_terms($data['competition'], $data['season']);
+    }
+}
 
-new API_Route($route, $callback);
+new API_Route('/competitions/standings/(?P<competition>\d+)/(?P<season>\d+)', [new League_API_Controller, 'get_standings_by_terms']);
