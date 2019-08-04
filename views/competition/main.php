cbf51375ec5ef83fec0e71c7b71f78e29e71fa31
Valerio Senneca <val.senneca@gmail.com>
2019-08-04 22:19:26 +0200

    Fixed last season selection

--- views/competition/main.php
+++ views/competition/main.php
@@ -6,16 +6,11 @@ $season_slug = get_query_var('season', false);
 
 if ($season_slug) $season_term = get_term_by('slug', $season_slug, 'seasons');
 
-if (!$season_term) {
-    $season_term = get_terms([
-        'taxonomy' => 'seasons',
-        'orderby' => 'name',
-        'order' => 'DESC',
-        'number' => 1
-    ])[0];
-}
-
 $competition_term = get_term_by('slug', 'competition-' . $post->ID, 'competitions_taxonomy');
+$all_seasons = Competition_Controller::get_all_seasons($competition_term);
+
+if (!$season_term) $season_term = get_term_by('slug', array_keys($all_seasons)[0], 'seasons');
+
 $competitions = array_map(function($competition) {
     $competition->turns = array_map(function($turn) {
         $turn["id"] = $turn["name"];
@@ -25,8 +20,6 @@ $competitions = array_map(function($competition) {
     if ($competition->type === 'leagues') return League_Controller::get_standings($competition);
     return $competition;
 }, Competition_Controller::get_by_terms($competition_term->term_id, $season_term->term_id));
-
-$all_seasons = Competition_Controller::get_all_seasons($competition_term);
 ?>
 
 <div class="page-competition row--boxed">
