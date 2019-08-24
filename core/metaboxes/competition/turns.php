0507201bb71efcd73e55251286af844f284f0d42
Valerio Senneca <val.senneca@gmail.com>
2019-08-24 16:29:30 +0200

    Reset

--- core/metaboxes/competition/turns.php
+++
@@ -1,8 +0,0 @@
-<?php
-use TBI\Models\Core\Metabox;
-
-new Metabox([
-    'id' => 'competition-turns',
-    'title' => 'Calendario',
-    'post_types' => ['leagues', 'cups']
-]);
--- header.php
+++
@@ -1,8 +0,0 @@
-<html> <?php
-    include "views/public/layout/header/head.php"; ?>
-
-    <body>
-        <div id="tbi-vue">
-            <header> <?php
-                include "views/public/layout/header/menu.php"; ?>
-            </header>
